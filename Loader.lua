3a506cabcddfd96e63f3a5519a526fcda216f79d
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2023-07-06 19:31:30 +0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 11981520251 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/HeavenStand.lua"))()
+else
+    game.Players.LocalPlayer:Kick("This experience is not supported.")
+end
