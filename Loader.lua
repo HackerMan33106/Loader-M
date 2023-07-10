76c36edf7a63e2bb9da7b47f7c5ea53de2c9ba55
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2023-07-10 23:22:09 +0300

    Update Loader.lua
    +Add script to Golden Records

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 11981520251 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/HeavenStand.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/HeavenStand.lua"))()
+elseif game.PlaceId == 6910146292 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/GoldenRecords.lua"))()
 else
-    game.Players.LocalPlayer:Kick("This experience is not supported.")
+	game.Players.LocalPlayer:Kick("This experience is not supported.")
 end
