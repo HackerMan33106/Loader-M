9f504525486fc78fa6c2a8e7b8bf7dab173206fe
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2023-07-14 13:35:07 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,8 @@ if game.PlaceId == 11981520251 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/HeavenStand.lua"))()
 elseif game.PlaceId == 6910146292 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/GoldenRecords.lua"))()
+elseif game.PlaceId == 9821272782 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/StardustReborn.lua"))()
 else
 	game.Players.LocalPlayer:Kick("This experience is not supported.")
 end
