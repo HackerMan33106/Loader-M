c625e83433d33e2769f4d8117c59435843404ed4
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2024-05-23 20:00:48 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,5 +5,8 @@ elseif game.PlaceId == 6910146292 then
 elseif game.PlaceId == 9821272782 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/StardustReborn.lua"))()
 else
-	game.Players.LocalPlayer:Kick("This experience is not supported.")
+	game.Players.LocalPlayer.Character.Humanoid.PlatformStand = false
+end
+if game.Players.LocalPlayer.Character.Humanoid.PlatformStand == true then
+	game.Players.LocalPlayer:Kick("Don't even try it.")
 end
