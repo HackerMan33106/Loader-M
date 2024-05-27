85a6ce574ab1f3ddeb5e7dfeb6809ab7a9251f03
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2024-05-28 00:17:40 +0300

    Update LoaderLanguageSelection.lua

--- LanguageSelection/LoaderLanguageSelection.lua
+++ LanguageSelection/LoaderLanguageSelection.lua
@@ -1,13 +1,17 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 local Window = Library.CreateLib("Language selection", "RJTheme5")
 
---- 1 Section Teleporting
+-- 1 Section
 local Tab = Window:NewTab("Language selection")
-local Section = Tab:NewSection("Language - Язык")
+local Section = Tab:NewSection("Language")
 
 Section:NewButton("English", "English language selection in cheat.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/Loader-M/main/Loader.lua", true))()
 end)
+
+-- 2 Section
+local Tab = Window:NewTab("Выбор языка")
+local Section = Tab:NewSection("Язык")
 
 Section:NewButton("Русский", "Выбор русского языка в чите.", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/Loader-M/main/LoaderRu.lua", true))()
