693e996ef29cf45aa487febbf093ba6c9aad925d
HackerMan <138781082+HackerMan33106@users.noreply.github.com>
2024-06-14 01:16:44 +0300

    Update LoaderRu.lua

--- LoaderRu.lua
+++ LoaderRu.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 14561998168 then
 elseif game.PlaceId == 6910146292 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/ru-translate/GoldenRecordsRu.lua"))()
 elseif game.PlaceId == 9821272782 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/ru-translate/StardustReborn.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/HackerMan33106/main/main/ru-translate/StardustRebornRu.lua"))()
 else
 	game.Players.LocalPlayer:Kick("Этот режим не поддерживается.")
 end
